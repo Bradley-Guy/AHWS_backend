88eae8f7909c1aa92d4be6acd4f70baa71d5b3d9
Bradley Guy <1.chartcove@gmail.com>
2024-10-03 14:11:54 -0500

    allow all origins

--- server.js
+++ server.js
@@ -58,12 +58,12 @@ app.use(express.json());
 //     ],
 //     methods: ['GET', 'POST', 'PUT', 'DELETE'],
 // }));
-// app.use((req, res, next) => {
-//     res.header('Access-Control-Allow-Origin', '*'); // Allow all origins
-//     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-//     res.header('Access-Control-Allow-Headers', 'Content-Type');
-//     next();
-// });
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*'); // Allow all origins
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
 // Middleware to log all incoming requests
 app.all('/', function(req, res, next) {
     //console.log({method: req.method, url: req.url});
